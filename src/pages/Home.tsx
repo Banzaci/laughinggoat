bc69929031dc13b58d606b87c1ff749956d1f966
CORP\michlars <michel.larsson@tele2.se>
2021-06-30 11:27:13 +0200

    WIP

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Main } from './components/Components';
 import Hero from './components/Hero';
+import House from './components/house.jpg';
 
 function Home() {
   return (
     <Main>
-       <Hero />
+       <Hero src={ House } />
     </Main>
   );
 }
--- src/pages/components/Hero.tsx
+++ src/pages/components/Hero.tsx
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import House from './house.jpg';
 
-const Wrapper = styled.div`
-  background-image: url('${House}');
+const Wrapper = styled.div<{ src: any; }>`
+  background-image: url('${props => props.src}');
   background-repeat: no-repeat;
   background-size: cover;
   width: 100%;
   height: 100%;
 `;
 
-function Hero() {
-  return (
-    <Wrapper />
-  );
+interface Props {
+  src: unknown;
 }
+
+function Hero({ src }: Props) {
+  const [ source, setSource ] = useState('')
+
+  useEffect(() => {
+      const img = new Image()
+      img.src = House
+      img.onload = () => setSource(House)
+  }, [src])
+
+  return(
+      <Wrapper src={ source } />
+  )
+}
+
 export default Hero;
