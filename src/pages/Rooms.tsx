e2bc21aa0af5187acaafe78e90d39e09776c8c18
Banzaci <c.michel.larsson@northvolt.com>
2022-12-06 08:44:42 +0100

    Add text

--- src/pages/Rooms.tsx
+++ src/pages/Rooms.tsx
@@ -4,9 +4,9 @@ import { Main , Wrapper, Content, FontWrapper } from './components/Components';
 import Text from './components/Text';
 import Hero from './components/Hero';
 import ThreeImages, { TextImages } from './components/ThreeImages';
-// import Room1 from '../imgs/room1.jpg';
-// import Room2 from '../imgs/room2.jpg';
-// import Room3 from '../imgs/room3.jpg';
+import Room1 from '../imgs/room1.jpg';
+import Room2 from '../imgs/room2.jpg';
+import Room3 from '../imgs/room3.jpg';
 import Room4 from '../imgs/room6.jpg';
 import Room5 from '../imgs/room5.jpg';
 import Room6 from '../imgs/room4b.jpg';
@@ -18,38 +18,41 @@ export const roomIngress = [
   'High season is 22nd of December to 12th of January, 1st of March to 5th of April',
 ];
 
-// export const roomsRow1: TextImages[] = [
-//   {
-//     ingress: 'Room on first floor with one double bed, a bunker bed and private bathroom.',
-//     capacity: '4 people',
-//     price: 220,
-//     image: Room1,
-//     priceSurfcampWeekend: 270,
-//     priceSurfcampWeek: 480,
-//   },
-//   {
-//     ingress: 'Room on first floor one regular bed, a bunker bed and private bathroom.',
-//     capacity: '3 people',
-//     price: 200,
-//     image: Room2,
-//     priceSurfcampWeekend: 270,
-//     priceSurfcampWeek: 480,
-//   },
-//   {
-//     ingress: 'Room on first floor with one regular bed with shared bathroom.',
-//     capacity: '1 person',
-//     price: 100,
-//     image: Room3,
-//     priceSurfcampWeekend: 270,
-//     priceSurfcampWeek: 480,
-//   }
-// ];
+export const roomsRow1: TextImages[] = [
+  {
+    ingress: 'Room on first floor with one double bed, a bunker bed and private bathroom.',
+    capacity: '2 people',
+    price: 170,
+    image: Room1,
+    priceSurfcampWeekend: 270,
+    priceSurfcampWeek: 480,
+    priceHighSeason: 270,
+  },
+  {
+    ingress: 'Room on first floor one regular bed, a bunker bed and private bathroom.',
+    capacity: '2 people',
+    price: 170,
+    image: Room2,
+    priceSurfcampWeekend: 270,
+    priceSurfcampWeek: 480,
+    priceHighSeason: 270,
+  },
+  {
+    ingress: 'Room on first floor with one regular bed with shared bathroom.',
+    capacity: '1 person',
+    price: 100,
+    image: Room3,
+    priceSurfcampWeekend: 270,
+    priceSurfcampWeek: 480,
+    priceHighSeason: 140,
+  }
+];
 
 export const roomsRow2: TextImages[] = [
   {
     ingress: 'Room on second floor with one double bed and private bathroom',
     capacity: '2 people',
-    price: 180,
+    price: 170,
     image: Room4,
     priceSurfcampWeekend: 270,
     priceSurfcampWeek: 480,
@@ -67,7 +70,7 @@ export const roomsRow2: TextImages[] = [
   {
     ingress: 'Room on second floor with one double bed and private bathroom',
     capacity: '2 people',
-    price: 180,
+    price: 170,
     image: Room6,
     priceSurfcampWeekend: 270,
     priceSurfcampWeek: 480,
@@ -85,8 +88,8 @@ function Rooms() {
             <Text smallMargin left text={ roomIngress[0] }/>
             <Text smallMargin left text={ roomIngress[1] }/>
             <Text smallMargin left text={ roomIngress[2] }/>
-            {/* <ThreeImages textImages={ roomsRow1 } rooms /> */}
             <ThreeImages textImages={ roomsRow2 } rooms />
+            <ThreeImages textImages={ roomsRow1 } rooms />
           </Content>
        </Wrapper>
     </Main>
